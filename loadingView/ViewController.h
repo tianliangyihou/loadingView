7ae7d3fd3d392fff27c056e5b080e0745b7c069f
llb <llb@llbdeimac.local>
2016-10-05 10:37:09 +0800

    Initial Commit

---
+++ loadingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  loadingView
+//
+//  Created by llb on 16/10/5.
+//  Copyright © 2016年 llb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ loadingView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  loadingView
+//
+//  Created by llb on 16/10/5.
+//  Copyright © 2016年 llb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
